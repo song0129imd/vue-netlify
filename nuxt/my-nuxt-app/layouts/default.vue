3e278665a76be128d6f5cb46ac4a7155e6681b5d
Qing Song <song0129@algonquinlive.com>
2023-11-29 16:45:13 -0500

    creates netlify

---
+++ nuxt/my-nuxt-app/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink :to="`/pokemon/${1}`">Pokemon</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+    <slot />
+  </section>
+</template>
+
+<script></script>
+
+<style></style>
---
+++ nuxt/my-nuxt-app/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>My main view from the index.vue</h1>
+    <NuxtLink :to="`/pokemon/${1}`">Pokemon</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
