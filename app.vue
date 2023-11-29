6d66d84890e2a43278106beef3ac6749267d06ed
Qing Song <song0129@algonquinlive.com>
2023-11-29 17:34:53 -0500

    creates git deploy

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <MyComponent />
+    <!-- <NuxtWelcome /> -->
+    <NuxtLayout>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ components/MyComponent.vue
@@ -0,0 +1,8 @@
+<template>
+  <h2>Here is my component</h2>
+</template>
+
+<script>
+export default {};
+</script>
+<style></style>
