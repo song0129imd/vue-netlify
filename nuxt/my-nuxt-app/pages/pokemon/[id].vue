42f217ab50bbacac173a5337599108fbf87955b2
Qing Song <song0129@algonquinlive.com>
2023-11-29 17:18:58 -0500

    creates public

--- nuxt/my-nuxt-app/pages/pokemon/[id].vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h1>This is my pokemon route with the parameter {{ $route.params.id }}</h1>
-    <img
-      :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${$route.params.id}.png`"
-      alt="pokemon sprite"
-    />
-  </div>
-</template>
